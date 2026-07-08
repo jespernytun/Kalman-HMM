9be296039f6d91dfacd931230b24ff54c40b184d
Jesper Nytun <jeppe@Jespers-MacBook-Pro.local>
2026-07-08 19:49:28 +0200

    added dataclasses for next part

---
+++ src/kalman_filter.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from dataclasses import dataclass
+
+
+# Our states
+@dataclass
+class KalmanState:
+    price: float # price right now
+    trend: float # derivate
+
+# Dataclass representing the filter itself
+# Q the performance- and R the cost matrix
+@dataclass
+class KalmanMatrices:
+    Q: np.array
+    R: float
+
+    ## We create a function that let's us create the 
+    @classmethod
+    def create(cls, q_price: float, q_trend: float, r:float) --> "KalmanMatrices":
+        q = np.array([[q_level, 0.0],
+                      [0.0,     q_trend]])
+        return cls(Q=q, R=r)
