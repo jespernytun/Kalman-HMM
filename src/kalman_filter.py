ea0d47e38891c4a641605afdb6635febec529d6e
Jesper Nytun <jeppe@Jespers-MacBook-Pro.local>
2026-07-08 20:42:02 +0200

    a predict function i hope works

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 class KalmanState:
     price: float # price right now
     trend: float # derivate
+    P: np.ndarray # state covariance
 
 # Dataclass representing the filter itself
 # Q the performance- and R the cost matrix
@@ -21,3 +22,28 @@ class KalmanMatrices:
         q = np.array([[q_price, 0.0],
                       [0.0,     q_trend]])
         return cls(Q=q, R=r)
+
+# Creating the filter logic, as inspired by the filterpy.kalman github repo
+
+# Predicts the next state using the kalman equations
+def predict(state: KalmanState, F: np.ndarray, noise: KalmanMatrices, dt: float) -> KalmanState:
+
+    # If we assume that our x = [price, trend], and trend is a constant (for simplification) we get
+    # x_dot = A@x,   A = [0, dt; 0, 0]
+    # x_t = F*x_t-1, F = [1, dt; 0, 1]
+
+    # if ever I find our that trend is not constant I add it as a optional argument
+    if F is None:
+        F = np.array([[1, dt], [0, 1]])
+
+    # We calculate the predition
+    x = np.array([state.price, state.trend])
+
+    x_prior = F@x
+    P_prior = F@state.P@F.T + noise.Q
+
+    return KalmanState(price=x_prior[0], trend=x_prior[1], P=P_prior)
+
+#def update(state: KalmanState, observation: float, H: np.ndarray, noise: KalmanMatrices) -> KalmanState:
+
+#def run_kalman(prices: np.ndarray, noise: KalmanMatrices) -> KalmanResult:
